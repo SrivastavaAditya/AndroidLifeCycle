d839ab5e53f7c9786ccf4b2b73f127118c2cbcc9
SrivastavaAditya <aditya.srivastava@svam.com>
2019-03-12 14:19:13 +0530

    added lifecycle methods in main activity

---
+++ app/src/main/java/com/example/adityasrivastava/androidlifecycleapp/activities/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.adityasrivastava.androidlifecycleapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.adityasrivastava.androidlifecycleapp.R;
+
+/**
+ * The type Main activity.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
