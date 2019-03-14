43d9aa01bd668b2618d6d11aafb381e0aed94a81
SrivastavaAditya <aditya.srivastava@svam.com>
2019-03-14 12:36:39 +0530

    fragment lifecycle implemented

--- app/src/main/java/com/example/adityasrivastava/androidlifecycleapp/activities/MainActivity.java
+++ app/src/main/java/com/example/adityasrivastava/androidlifecycleapp/activities/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.example.adityasrivastava.androidlifecycleapp.R;
+import com.example.adityasrivastava.androidlifecycleapp.fragments.FirstFragment;
 import com.example.adityasrivastava.androidlifecycleapp.utils.Utility;
 
 /**
@@ -21,48 +22,58 @@ public class MainActivity extends AppCompatActivity {
         parentLayout = findViewById(R.id.parent);
 
         parentLayout.setBackgroundColor(getResources().getColor(R.color.onCreate));
-        Utility.showMessage(this, getResources().getString(R.string.on_create_method_called));
+
+        getSupportFragmentManager().beginTransaction().add(android.R.id.content, new FirstFragment())
+                .addToBackStack(null).commit();
+        Utility.showMessage(this, getResources()
+                .getString(R.string.main_on_create_method_called));
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         parentLayout.setBackgroundColor(getResources().getColor(R.color.onStart));
-        Utility.showMessage(this, getResources().getString(R.string.on_start_method_called));
+        Utility.showMessage(this, getResources()
+                .getString(R.string.main_on_start_method_called));
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         parentLayout.setBackgroundColor(getResources().getColor(R.color.onResume));
-        Utility.showMessage(this, getResources().getString(R.string.on_resume_method_called));
+        Utility.showMessage(this, getResources()
+                .getString(R.string.main_on_resume_method_called));
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
         parentLayout.setBackgroundColor(getResources().getColor(R.color.onRestart));
-        Utility.showMessage(this, getResources().getString(R.string.on_restart_method_called));
+        Utility.showMessage(this, getResources()
+                .getString(R.string.main_on_restart_method_called));
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         parentLayout.setBackgroundColor(getResources().getColor(R.color.onPause));
-        Utility.showMessage(this, getResources().getString(R.string.on_pause_method_called));
+        Utility.showMessage(this, getResources()
+                .getString(R.string.main_on_pause_method_called));
     }
 
     @Override
     protected void onStop() {
         super.onStop();
         parentLayout.setBackgroundColor(getResources().getColor(R.color.onStop));
-        Utility.showMessage(this, getResources().getString(R.string.on_stop_method_called));
+        Utility.showMessage(this, getResources()
+                .getString(R.string.main_on_stop_method_called));
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         parentLayout.setBackgroundColor(getResources().getColor(R.color.onDestroy));
-        Utility.showMessage(this, getResources().getString(R.string.on_destroy_method_called));
+        Utility.showMessage(this, getResources()
+                .getString(R.string.main_on_destroy_method_called));
     }
 }
--- app/src/main/java/com/example/adityasrivastava/androidlifecycleapp/fragments/FirstFragment.java
+++ app/src/main/java/com/example/adityasrivastava/androidlifecycleapp/fragments/FirstFragment.java
@@ -3,6 +3,7 @@ package com.example.adityasrivastava.androidlifecycleapp.fragments;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.adityasrivastava.androidlifecycleapp.R;
+import com.example.adityasrivastava.androidlifecycleapp.utils.Utility;
 
 public class FirstFragment extends Fragment {
 
@@ -20,52 +22,79 @@ public class FirstFragment extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
+        Utility.showMessage(getContext(), getResources()
+                .getString(R.string.first_fragment_on_attach_method_called));
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Utility.showMessage(getContext(), getResources()
+                .getString(R.string.first_fragment_on_create_method_called));
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        Utility.showMessage(getContext(), getResources()
+                .getString(R.string.first_fragment_on_create_view_method_called));
         return inflater.inflate(R.layout.fragment_my, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Utility.showMessage(getContext(), getResources()
+                .getString(R.string.first_fragment_on_view_created_method_called));
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        Utility.showMessage(getContext(), getResources()
+                .getString(R.string.first_fragment_on_activity_created_method_called));
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        Utility.showMessage(getContext(), getResources()
+                .getString(R.string.first_fragment_on_resume_method_called));
     }
 
     @Override
     public void onPause() {
         super.onPause();
+        Utility.showMessage(getContext(), getResources()
+                .getString(R.string.first_fragment_on_pause_method_called));
     }
 
     @Override
     public void onStop() {
         super.onStop();
+        Utility.showMessage(getContext(), getResources()
+                .getString(R.string.first_fragment_on_stop_method_called));
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+        Utility.showMessage(getContext(), getResources()
+                .getString(R.string.first_fragment_on_destroy_view_method_called));
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        Utility.showMessage(getContext(), getResources()
+                .getString(R.string.first_fragment_on_destroy_method_called));
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
+        Utility.showMessage(getContext(), getResources()
+                .getString(R.string.first_fragment_on_detach_method_called));
     }
 }
